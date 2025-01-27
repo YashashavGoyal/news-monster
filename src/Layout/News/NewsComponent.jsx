697c95f82c4755d12dda4bb989893db595724e77
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2025-01-27 19:07:45 +0530

    News component Created

--- src/Layout/News/NewsComponent.jsx
+++ src/Layout/News/NewsComponent.jsx
@@ -4,12 +4,25 @@ import NewsItem from '../../Components/NewsItem/NewsItem'
 export default class NewsComponent extends Component {
 
 
-
     render() {
         return (
             <>
-                <div>NewsComponent</div>
-                <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
+                <div className='container mx-5 my-3'>
+                    <div className='fs-3 text-center my-3 fw-bolder'>
+                        News Monster - Top News Headline
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
+                        </div>
+                        <div className="col-md-4">
+                            <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
+                        </div>
+                        <div className="col-md-4">
+                            <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
