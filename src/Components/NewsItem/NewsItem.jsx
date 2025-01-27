2107edadce72442e7f24dc711a55cff1ee999c50
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2025-01-27 22:27:32 +0530

    fetching top news headlines

--- src/Components/NewsItem/NewsItem.jsx
+++ src/Components/NewsItem/NewsItem.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
 export default class NewsItem extends Component {
-    
+
     render() {
 
         let {title, description, urlToImage, url} = this.props;
 
         return (
             <>
-                <div className="card" style={{width: '18rem'}}>
+                <div className="card m-2" style={{width: '18rem'}}>
                     <img src={urlToImage} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href={url} className="btn btn-primary">Go To Actual Article</a>
+                            <h6 className="card-title">{title}...</h6>
+                            <p className="card-text">{description}...</p>
+                            <a href={url} target='_blank' className="btn btn-primary btn-sm">Go To Actual Article</a>
                         </div>
                 </div>
             </>
--- src/Layout/News/NewsComponent.jsx
+++ src/Layout/News/NewsComponent.jsx
@@ -4,6 +4,49 @@ import NewsItem from '../../Components/NewsItem/NewsItem'
 export default class NewsComponent extends Component {
 
 
+    apiKey = '32ef4db84abf40de9525a1616da0c9c3';
+    url = 'https://newsapi.org/v2/top-headlines'; // For Top Heading
+
+    async topNewsHeadLine() {
+        const URI = `${this.url}?apiKey=${this.apiKey}&language=en`;
+        this.setState({
+            loading: true,
+        });
+
+        try {
+            const response = await fetch(URI, {
+                method: 'GET',
+            });
+
+            const resData = await response.json();
+            if (resData.status === "ok") {
+                this.setState({
+                    articles: resData.articles,
+                })
+            }
+
+            this.setState({
+                loading: false,
+            });
+        } catch (err) {
+            this.setState({
+                loading: false,
+            });
+            
+            alert("Internal Server Error");
+        }
+    }
+
+
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+        }
+        this.topNewsHeadLine();
+    }
+
     render() {
         return (
             <>
@@ -12,15 +55,20 @@ export default class NewsComponent extends Component {
                         News Monster - Top News Headline
                     </div>
                     <div className="row">
-                        <div className="col-md-4">
-                            <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title='hii' decription='hii' url='/' urlToImage='/' />
-                        </div>
+                        {this.state.articles.map((article, index) => {
+                            const { title, description, urlToImage, url } = article;
+                            
+                            return (
+                                <div className="col-md-4" key={index} >
+                                    <NewsItem
+                                        title={title.slice(0,45)}
+                                        description={description.slice(0,88)}
+                                        urlToImage={urlToImage}
+                                        url={url}
+                                    />
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </>
